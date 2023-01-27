ec88d074a477357b14b39e02d57f9383fbda30ea
kramer9 <kramer92002@gmail.com>
2023-01-27 09:36:23 -0500

    Update popos.sh

--- popos.sh
+++ popos.sh
@@ -26,14 +26,14 @@ sudo systemctl enable pcscd ## for yubico authenticator
 sudo systemctl start pcscd ## for yubico authenticator
 
 sudo apt remove -y --purge libreoffice* ## remove libre in favor of onlyoffice
-sudo apt-get clean
-sudo apt-get autoremove
+sudo apt-get clean -y
+sudo apt-get autoremove -y
 flatpak list
-flatpak update
+flatpak update -y
 ## flatpak uninstall org.gimp.GIMP
 
 ##flatpak install flathub io.atom.Atom org.audacityteam.Audacity com.calibre_ebook.calibre org.gnome.DejaDup org.gnome.EasyTAG org.electrum.electrum  im.riot.Riot org.mozilla.firefox org.freefilesync.FreeFileSync org.gimp.GIMP org.gnucash.GnuCash fr.handbrake.ghb org.keepassxc.KeePassXC tv.kodi.Kodi com.getmailspring.Mailspring com.gitlab.newsflash org.onlyoffice.desktopeditors ch.protonmail.protonmail-bridge org.signal.Signal org.standardnotes.standardnotes com.github.micahflee.torbrowser-launcher com.transmissionbt.Transmission org.videolan.VLC com.wire.WireDesktop -y
-flatpak install flathub com.calibre_ebook.calibre org.mozilla.firefox org.freefilesync.FreeFileSync fr.handbrake.ghb org.onlyoffice.desktopeditors com.github.micahflee.torbrowser-launcher com.transmissionbt.Transmission org.videolan.VLC -y
+flatpak install flathub com.calibre_ebook.calibre org.mozilla.firefox org.freefilesync.FreeFileSync fr.handbrake.ghb org.onlyoffice.desktopeditors com.github.micahflee.torbrowser-launcher com.transmissionbt.Transmission org.videolan.VLC com.yubico.yubioath -y
 
 exit 0
 sudo snap install vlc
