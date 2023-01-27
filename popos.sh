b55ff9f113461c212e2b58b85bc0caf1955410a9
kramer9 <kramer92002@gmail.com>
2023-01-27 10:02:16 -0500

    Update popos.sh

--- popos.sh
+++ popos.sh
@@ -33,7 +33,7 @@ flatpak update -y
 ## flatpak uninstall org.gimp.GIMP
 
 ##flatpak install flathub io.atom.Atom org.audacityteam.Audacity com.calibre_ebook.calibre org.gnome.DejaDup org.gnome.EasyTAG org.electrum.electrum  im.riot.Riot org.mozilla.firefox org.freefilesync.FreeFileSync org.gimp.GIMP org.gnucash.GnuCash fr.handbrake.ghb org.keepassxc.KeePassXC tv.kodi.Kodi com.getmailspring.Mailspring com.gitlab.newsflash org.onlyoffice.desktopeditors ch.protonmail.protonmail-bridge org.signal.Signal org.standardnotes.standardnotes com.github.micahflee.torbrowser-launcher com.transmissionbt.Transmission org.videolan.VLC com.wire.WireDesktop -y
-flatpak install flathub com.calibre_ebook.calibre org.mozilla.firefox org.freefilesync.FreeFileSync fr.handbrake.ghb org.onlyoffice.desktopeditors com.github.micahflee.torbrowser-launcher com.transmissionbt.Transmission org.videolan.VLC com.yubico.yubioath -y
+flatpak install flathub com.calibre_ebook.calibre org.mozilla.firefox org.freefilesync.FreeFileSync fr.handbrake.ghb org.onlyoffice.desktopeditors com.github.micahflee.torbrowser-launcher com.transmissionbt.Transmission org.videolan.VLC com.yubico.yubioath com.visualstudio.code -y
 
 exit 0
 sudo snap install vlc
